498cc6cce1a9aabbdf359b00372f06a4396928e6
Piotr Maślanka <piotr.maslanka@henrietta.com.pl>
2022-01-14 19:46:21 +0100

    add swagger

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,8 @@
 FROM python:3.9
 
 ADD requirements.txt /tmp/requirements.txt
-RUN pip install -f /tmp/requirements.txt
+RUN cat /tmp/requirements.txt
+RUN pip install -r /tmp/requirements.txt
 
 
 WORKDIR /app
@@ -9,5 +10,6 @@ RUN mkdir /app/static
 ADD chad /app/chad
 ADD manage.py /app/manage.py
 
+RUN python manage.py collectstatic
 
 CMD ["gunicorn", "-w", "4", "-b", "0.0.0.0:8000", "chad.wsgi:application"]
--- chad/settings.py
+++ chad/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-t_v=skzy15gadvzn_xf%c(u$8a*m4^r!o-@-74$aq_f!)b4!6j'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', '1') == '1'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'drf_spectacular',
 ]
 
 MIDDLEWARE = [
